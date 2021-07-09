38d417a12433acac8bd20211e9dae1778164fe84
Roz <rosalindavalbrecht@gmail.com>
2021-07-08 22:02:17 -0700

    Renders roll info tables

--- components/ClassTableComponent.js
+++ components/ClassTableComponent.js
@@ -1,23 +1,72 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Card, Divider } from "react-native-elements";
 
-function RenderClassTable({ hustle }) {
+function ClassTable({ hustle }) {
   if (hustle) {
+    function HustleItem({ tbl }) {
+      if (tbl) {
+        hustle.tableArr.map((hustleItem, index) => {
+          return <Text key={index}>{hustleItem.description}</Text>;
+        });
+      }
+      return <View />;
+    }
+
     return (
-      <Card
-        featuredTitle={hustle.name}
-        image={require("../assets/images/rockerboy.png")}
-      >
-        <Text style={{ margin: 10 }}>{hustle.roll1[0]}</Text>
-      </Card>
+      <ScrollView>
+        <Card
+          featuredTitle={hustle.name}
+          image={require("../assets/images/rockerboy.png")}
+        >
+          {hustle.tableArr.map((hustleItem, index) => {
+            return (
+              <View key={index}>
+                <Text style={styles.description}>{hustleItem.description}</Text>
+                <View style={styles.lvlContainer}>
+                  <Text style={styles.lvl1}>
+                    Levels 1-4: {hustleItem.level1}
+                  </Text>
+                  <Text style={styles.lvl2}>
+                    Levels 5-7: {hustleItem.level2}
+                  </Text>
+                  <Text style={styles.lvl3}>
+                    Levels 8-10: {hustleItem.level3}
+                  </Text>
+                </View>
+                <Divider orientation="horizontal" style={{ marginTop: 10 }} />
+              </View>
+            );
+          })}
+        </Card>
+      </ScrollView>
     );
   }
   return <View />;
 }
-
+/* 
 function ClassTable(props) {
   return <RenderClassTable hustle={props.hustle} />;
 }
+ */
 
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    marginTop: 15,
+  },
+  lvlContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  lvl1: {
+    color: "red",
+  },
+  lvl2: {
+    color: "orange",
+  },
+  lvl3: {
+    color: "green",
+  },
+});
 export default ClassTable;
